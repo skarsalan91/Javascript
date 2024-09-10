5934ed0cb1c902833af29b5d53aa56203dec9230
Shaikh Arsalan <shaikharsalan123@gmail.com>
2024-09-10 15:15:48 +0530

    array practice

---
+++ array.js
@@ -0,0 +1,42 @@
+// const myArray = [1,2,3,4,5]
+
+// const myNewArray = new Array(1,2,3,4)
+ 
+// console.log(myArray[1])
+
+// console.log(myNewArray[3])
+
+// myNewArray.push(7);    //insert at the end of array 
+
+// console.log(myNewArray)
+
+// myNewArray.pop(); // Delete the first array
+//myNewArray.shift();
+
+//console.log(myNewArray)
+//-----------------------merge of 2 array example 1
+// function mergeArrays(arr1,arr2)
+// {
+//     return arr1.concat(arr2)
+// }
+
+
+// const array1 = [1,2,3,4]
+// const array2 = [5,6,7,8]
+
+// console.log(mergeArrays(array1,array2))
+//--------------------------merge of 2 array example 2
+
+// const array3 = [1,2,3,4]
+// const array4 = [5,6,7,8]
+
+// console.log(array1.concat(array2))
+
+//-----------------find maximum array
+// function findMax(arr)
+// {
+//     return Math.max(...arr);
+// }
+// const arrMaxValue = [2,4,12,33];
+// console.log(findMax(arrMaxValue));
+//-------------------------Array example--------------------------------//
